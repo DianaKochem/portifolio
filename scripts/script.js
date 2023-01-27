bece87e7cd8f05d86a2c4a92c6d252dd150b2194
Diana Kochem <dianakochem24@gmail.com>
2023-01-27 17:07:29 -0300

    Revert "Atualização de resposividade"
    This reverts commit 531fff797551255cb26ec51de2eafec12e441243.

--- scripts/script.js
+++ scripts/script.js
@@ -25,12 +25,3 @@ function includeHTML() {
         }
     }
 };
-
-function clickMenu() {
-    if (menu.style.display == 'block') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
-    }
-
-}
